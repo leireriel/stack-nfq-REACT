43144ef4269531edd40d6bdc26052e9dd8acacaf
AiaRup <aiale88@gmail.com>
2019-05-31 18:32:31 +0200

    add routing to app, start component questionDetails

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from '../Home';
+// import StackList from '../StackList';
+import QuestionDetails from '../QuestionDetails';
+// import TeamInfo from '../TeamInfo';
 
 class App extends Component {
   render() {
-    return <div className="">hola</div>;
+    return (
+      <Switch>
+        <Route exact path="/home" render={() => <Home />} />
+        {/* <Route exact path="/questions" render={() => <StackList />} /> */}
+        <Route exact path="/question/:id" render={routeProps => <QuestionDetails id={routeProps.match.params.id} />} />
+        {/* <Route exact path="/card" render={() => <TeamInfo />} /> */}
+        <Redirect from="/" to="/home" />
+      </Switch>
+    );
   }
 }
 
--- src/components/QuestionDetails/index.js
+++ src/components/QuestionDetails/index.js
@@ -1,1 +1,8 @@
 import './styles.scss';
+import React from 'react';
+
+const QuestionDetails = props => {
+  return <div className="question__details-page">question details: {props.id}</div>;
+};
+
+export default QuestionDetails;
