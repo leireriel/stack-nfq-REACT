a3d72c18f5d056cdb7adad0166b3002e9ed78983
leireriel <leirericop@gmail.com>
2019-05-31 16:28:54 +0200

    Fetch function created in App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import './styles.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getQuestion = this.getQuestion.bind(this);
+  }
+  componentDidMount() {
+    this.getQuestion()
+  }
+
+  getQuestion() {
+
+  }
+
   render() {
     return <div className="">hola</div>;
   }
