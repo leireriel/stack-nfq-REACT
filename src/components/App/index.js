4e37878d755ab1abd6137ede293ca63c4d2e2217
AiaRup <aiale88@gmail.com>
2019-05-31 16:04:07 +0200

    add basic structure

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return <div className="">hola</div>;
+  }
+}
+
+export default App;
