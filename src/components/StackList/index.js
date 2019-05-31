405a26f7e4dfa32ed588c505e081a62b21626b41
crod2 <carlarodriguez90@gmail.com>
2019-05-31 18:37:43 +0200

    Create function for opening dialogue

--- src/components/StackList/index.js
+++ src/components/StackList/index.js
@@ -3,6 +3,21 @@ import ModalInfo from './ModalInfo';
 import './styles.scss';
 
 export default class StackList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false;
+    }
+
+    this.openDialogue = this.openDialogue.bind(this)
+  }
+
+  openDialogue() { 
+    this.setState( { isOpen : true } );
+
+  }
+
   render() {
     return (
       <div>
