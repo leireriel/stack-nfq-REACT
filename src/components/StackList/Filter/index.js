a034ada66d63aa63ead6184b783092a50012a83e
leireriel <leirericop@gmail.com>
2019-06-02 20:43:40 +0200

    Create filter component

--- src/components/StackList/Filter/index.js
+++ src/components/StackList/Filter/index.js
@@ -1,1 +1,14 @@
+import React, { Component } from 'react';
 import './styles.scss';
+
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        fhsdkj
+      </div>
+    );
+  }
+}
+
+export default Filter;
