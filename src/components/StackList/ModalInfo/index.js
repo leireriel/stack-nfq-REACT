c14ec8040aa56a1a90cf1ccfb5d4b778c2fa7668
crod2 <carlarodriguez90@gmail.com>
2019-06-02 21:14:23 +0200

    Change input states

--- src/components/StackList/ModalInfo/index.js
+++ src/components/StackList/ModalInfo/index.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import './styles.scss';
-// import PropTypes from 'prop-types';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Typography from '@material-ui/core/Typography';
-// import Modal from '@material-ui/core/Modal';
-// import Button from '@material-ui/core/Button';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -16,72 +11,56 @@ class ModalInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      helperText: '',
-      errorInfo: {
-        name: false,
-        tags: false,
-        title: false,
-        details: false
-      }
+      error: {
+        name: '',
+        tags: '',
+        title: '',
+        details: ''
+      } 
     };
+    
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event, infoInput) {
-    if (event.target.value.length) {
-      this.setState(prevState => {
-        let errorField = { ...prevState.errorInfo };
-        errorField[infoInput] = false;
-        return { helperText: '', errorInfo: errorField };
-      });
-    } else {
-      this.setState({ helperText: 'Please fill this field', error: true });
-    }
-  }
+  handleChange = inputInfo => event => {
+    const inputValue = event.target.value;
+    this.setState(prevState => {
+      const newError = { ...prevState.error, [inputInfo] : inputValue};
+      return { error: newError };
+    });
+  };
 
   render() {
-    const { helperText, errorInfo } = this.state;
+    const { name, tags, title, details } = this.state;
     const { dialogueFunction } = this.props;
     return (
       <Dialog open={true} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Añade una nueva pregunta</DialogTitle>
         <DialogContent>
           <TextField
-            helperText={helperText}
-            onChange={event => {
-              this.handleChange(event, 'name');
-            }}
-            error={errorInfo.name}
+            onChange={this.handleChange('name')}
+            value={name}
             required
             id="outlined-required"
             label="Nombre Usuario"
           />
           <TextField
-            helperText={helperText}
-            onChange={event => {
-              this.handleChange(event, 'tags');
-            }}
-            error={errorInfo.tags}
+            onChange={this.handleChange('tags')}
+            value={tags}
             required
             id="outlined-required"
             label="Tags"
           />
           <TextField
-            helperText={helperText}
-            onChange={event => {
-              this.handleChange(event, 'title');
-            }}
-            error={errorInfo.title}
+            onChange={this.handleChange('title')}
+            value={title}
             required
             id="outlined-required"
             label="Titulo Pregunta"
           />
           <TextField
-            helperText={helperText}
-            onChange={event => {
-              this.handleChange(event, 'details');
-            }}
-            error={errorInfo.details}
+            onChange={this.handleChange('details')}
+            value={details}
             required
             id="outlined-required"
             label="Detalle Pregunta"
@@ -96,13 +75,6 @@ class ModalInfo extends Component {
           </Button>
         </DialogActions>
       </Dialog>
-
-
-      // <Modal open={true}>
-      //   <form noValidate autoComplete="off">
-          
-      //   </form>
-      // </Modal>
     );
   }
 }
