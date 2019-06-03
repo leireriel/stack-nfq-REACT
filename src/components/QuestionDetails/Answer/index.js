b7d81c41aa51e711e9f6e2137cef0d324fb411f2
libertadcc <libertadchapinalcervantes@gmail.com>
2019-06-03 15:57:12 +0200

    More Likes

--- src/components/QuestionDetails/Answer/index.js
+++ src/components/QuestionDetails/Answer/index.js
@@ -4,21 +4,51 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 
-export default function Answer({ classPaper, answers }) {
-  return (
-    <Grid item xs={12}>
-      <Paper className={classPaper}>
-        <div className="answer__heart">
-          <span>{answers.likes}</span>
-        </div>
-        <h3 className="answer__text">{answers.answer}</h3>
-        <h4 className="answer__author">{answers.author}</h4>
-        <h5 className="answer__date">{answers.date}</h5>
-      </Paper>
-    </Grid>
-  );
+class Answer extends React.Component{
+  constructor(props){
+    super(props);
+    const {answers} =this.props;
+    this.state={
+      likes: parseInt(answers.likes)
+    }
+    this.updateLike=this.updateLike.bind(this);
+  }
+
+  updateLike(){
+    const {answers} =this.props;
+    const likes = parseInt(answers.likes);
+      this.setState((prevState, props) => {
+        let newLikes = {...likes};
+        newLikes = likes + 1;
+        console.log(newLikes)
+        return{
+          likes: newLikes
+        }
+      })
+  }
+
+  render(){
+    const { classPaper, answers } = this.props;
+    return (
+      <Grid item xs={12}>
+        <Paper className={classPaper}>
+          <div className="answer__heart">
+            <i className="fas fa-heart"
+             onClick={this.updateLike}>
+            {this.state.likes}
+            </i>
+          </div>
+          <h3 className="answer__text">{answers.answer}</h3>
+          <h4 className="answer__author">{answers.author}</h4>
+          <h5 className="answer__date">{answers.date}</h5>
+        </Paper>
+      </Grid>
+    );
+  }
 }
+export default Answer;
 
 Answer.propTypes = {
-  classPaper: PropTypes.string.isRequired
+  classPaper: PropTypes.string.isRequired,
+  answer: PropTypes.object
 };
