6cc6f9131b917f765ecf813adf6659145b695ce5
iris paredes <irisparedesjimenez@gmail.com>
2019-06-03 12:45:27 +0200

    fix headerHome styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,19 +2,47 @@ import './styles.scss';
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import Footer from '../Footer/index';
-import logoHome  from '../Images/logo-headerHome.svg';
+import logoHome  from '../Images/LOGO_blue.svg';
+import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
 
 
-class Home extends Component {
-  render() {
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 300,
+  },
+  media: {
+    height: 85,
+    width: 372,
+  },
+});
+
+function Home () {
+  
+    
+    
+    // function MediaCard() {
+      // const classes = useStyles();
     return (
       <div className="page__home">
-        <div className="container__header--home">
-        <img 
-          className="header__home" 
-          src={logoHome}
-          alt="Logotipo de nfq stack en página Home"/>
-        </div>
+        <header className="container__header--home">
+        {/* <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={logoHome}
+          width="372px"
+          title="Logotipo de nfq stack"
+        />
+        </Card> */}
+          <img 
+            className="header__home" 
+            src={logoHome}
+            // width="372px"
+            alt="Logotipo de nfq stack en página Home"
+            />
+          
+        </header>
         <div className="home__main">
           <h1 className="home__title">
             La respuesta que necesitas a tus problemas de desarrollo
@@ -29,6 +57,6 @@ class Home extends Component {
       </div>
     );
   }
-}
+
 
 export default Home;
