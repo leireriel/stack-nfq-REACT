8e50eb59d150a22228a8d593d6ce6413016f59da
crod2 <carlarodriguez90@gmail.com>
2019-06-03 13:03:07 +0200

    Minor fixes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ class Footer extends React.Component{
   render(){
     return(
       <div className="container__footer">
-        <a href="https://nfq.es/" target="_blank">
+        <a href="https://nfq.es/" target="_blank" rel="noopener noreferrer">
           <img 
             className="footer__nfq" 
             src={logoNFQ}
@@ -16,7 +16,7 @@ class Footer extends React.Component{
         <p className="footer__author">
           Diseñado por el equipo | © 2019
         </p>
-        <a href="https://adalab.es/"  target="_blank">
+        <a href="https://adalab.es/"  target="_blank" rel="noopener noreferrer">
           <img 
             className="footer__adalab" 
             src={logoAdalab} 
