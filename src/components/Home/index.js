52b8243be435235108156faac5c46fa5dcdf1ac4
iris paredes <irisparedesjimenez@gmail.com>
2019-06-03 12:54:21 +0200

    fix header stackList and delete comments

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,47 +1,18 @@
 import './styles.scss';
 import { Link } from 'react-router-dom';
-import React, { Component } from 'react';
+import React from 'react';
 import Footer from '../Footer/index';
-import logoHome  from '../Images/LOGO_blue.svg';
-import CardMedia from '@material-ui/core/CardMedia';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
+import logoHome  from '../Images/LOGO.svg';
 
-
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 300,
-  },
-  media: {
-    height: 85,
-    width: 372,
-  },
-});
-
-function Home () {
-  
-    
-    
-    // function MediaCard() {
-      // const classes = useStyles();
+function Home () {      
     return (
       <div className="page__home">
         <header className="container__header--home">
-        {/* <Card className={classes.card}>
-        <CardMedia
-          className={classes.media}
-          image={logoHome}
-          width="372px"
-          title="Logotipo de nfq stack"
-        />
-        </Card> */}
           <img 
             className="header__home" 
             src={logoHome}
-            // width="372px"
             alt="Logotipo de nfq stack en página Home"
             />
-          
         </header>
         <div className="home__main">
           <h1 className="home__title">
