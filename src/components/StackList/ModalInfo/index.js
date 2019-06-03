80e3dfa8be0da34461bb3e8315dac130e5fcc61b
crod2 <carlarodriguez90@gmail.com>
2019-06-03 18:20:22 +0200

    test styling for modal

--- src/components/StackList/ModalInfo/index.js
+++ src/components/StackList/ModalInfo/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import { withStyles, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -8,6 +10,31 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import moment from 'moment';
 import PropTypes from 'prop-types';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#4595cb'
+    },
+    secondary: {
+      main: '#777777'
+    }
+  }
+});
+
+const styles = theme => ({
+  modal: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  },
+  button: {
+    color: '#fff',
+    textTransform: 'capitalize',
+    fontWeight: 'bold',
+    margin: '0 20px',
+    padding: '10px 50px'
+  },
+  
+});
 
 class ModalInfo extends Component {
   constructor(props) {
@@ -19,14 +46,14 @@ class ModalInfo extends Component {
         title: '',
         details: ''
       },
-      error : {
+      error: {
         name: false,
         tags: false,
         title: false,
         details: false
       }
     };
-    
+
     this.handleChange = this.handleChange.bind(this);
     this.sendQuestion = this.sendQuestion.bind(this);
   }
@@ -34,97 +61,99 @@ class ModalInfo extends Component {
   handleChange = inputInfo => event => {
     const inputValue = event.target.value;
     this.setState(prevState => {
-      const newQuestion = { ...prevState.createQuestion, [inputInfo] : inputValue};
+      const newQuestion = { ...prevState.createQuestion, [inputInfo]: inputValue };
       const newError = { ...prevState.error, [inputInfo]: inputValue ? false : true };
-      return { error : newError, createQuestion : newQuestion };
+      return { error: newError, createQuestion: newQuestion };
     });
   };
 
   sendQuestion() {
     const newQuestion = {
-      ...this.state.createQuestion, answers : [], date: moment().format('YYYY-MM-DDTHH:MM:SS'), id: this.props.arrLength + 1
+      ...this.state.createQuestion, answers: [], date: moment().format('YYYY-MM-DDTHH:MM:SS'), id: this.props.arrLength + 1
     }
     this.props.createNewQuestion(newQuestion);
-    this.setState( {
-        createQuestion: {
-          name: '',
-          tags: '',
-          title: '',
-          details: ''
-        },
-        error : {
-          name: false,
-          tags: false,
-          title: false,
-          details: false
-        }
-      })
+    this.setState({
+      createQuestion: {
+        name: '',
+        tags: '',
+        title: '',
+        details: ''
+      },
+      error: {
+        name: false,
+        tags: false,
+        title: false,
+        details: false
+      }
+    })
     this.props.dialogueFunction();
-  } 
+  }
 
   render() {
-    const { createQuestion: {name, tags, title, details}, error } = this.state;
+    const { createQuestion: { name, tags, title, details }, error } = this.state;
     const { dialogueFunction } = this.props;
     return (
-      <Dialog open={true} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Añade una nueva pregunta</DialogTitle>
-        <DialogContent>
-          <TextField
-            onChange={this.handleChange('name')}
-            value={name}
-            required
-            label="Nombre Usuario"
-            error={error.name}
-            helperText={error.name ? 'Por favor, rellena este campo' : ''}
-            fullWidth
-          />
-          <TextField
-            onChange={this.handleChange('tags')}
-            value={tags}
-            required
-            label="Tags"
-            error={error.tags}
-            helperText={error.tags ? 'Por favor, rellena este campo' : ''}
-            fullWidth
-          />
-          <TextField
-            onChange={this.handleChange('title')}
-            value={title}
-            required
-            label="Titulo Pregunta"
-            error={error.title}
-            helperText={error.title ? 'Por favor, rellena este campo' : ''}
-            fullWidth
-          />
-          <TextField
-            onChange={this.handleChange('details')}
-            value={details}
-            required
-            label="Detalle Pregunta"
-            error={error.details}
-            helperText={error.details ? 'Por favor, rellena este campo' : ''}
-            fullWidth
-            multiline
-            rows='10'
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button color="primary" onClick={dialogueFunction}>
-            Cancelar
+      <ThemeProvider theme={theme}>
+        <Dialog open={true} aria-labelledby="form-dialog-title" PaperProps={{style: {backgroundColor: '#E8E8E8'}, overlaystyle: {backgroundColor: 'rgba(255, 255, 255, 0.5)'}}} >
+          <DialogTitle id="form-dialog-title" className="dialog__title" >Añade una nueva pregunta</DialogTitle>
+          <DialogContent>
+            <TextField
+              onChange={this.handleChange('name')}
+              value={name}
+              required
+              label="Nombre Usuario"
+              error={error.name}
+              helperText={error.name ? 'Por favor, rellena este campo' : ''}
+              fullWidth
+            />
+            <TextField
+              onChange={this.handleChange('tags')}
+              value={tags}
+              required
+              label="Tags"
+              error={error.tags}
+              helperText={error.tags ? 'Por favor, rellena este campo' : ''}
+              fullWidth
+            />
+            <TextField
+              onChange={this.handleChange('title')}
+              value={title}
+              required
+              label="Titulo Pregunta"
+              error={error.title}
+              helperText={error.title ? 'Por favor, rellena este campo' : ''}
+              fullWidth
+            />
+            <TextField
+              onChange={this.handleChange('details')}
+              value={details}
+              required
+              label="Detalle Pregunta"
+              error={error.details}
+              helperText={error.details ? 'Por favor, rellena este campo' : ''}
+              fullWidth
+              multiline
+              rows='10'
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={dialogueFunction}>
+              Cancelar
           </Button>
-          <Button color="primary" disabled={!name || !tags || !title || !details} onClick={this.sendQuestion}>
-            Aceptar
+            <Button color="primary" disabled={!name || !tags || !title || !details} onClick={this.sendQuestion}>
+              Aceptar
           </Button>
-        </DialogActions>
-      </Dialog>
+          </DialogActions>
+        </Dialog>
+      </ThemeProvider>
     );
   }
 }
 
 ModalInfo.propTypes = {
-  arrLength: PropTypes.arrayOf(PropTypes.number),
+  arrLength: PropTypes.number,
   createNewQuestion: PropTypes.func,
   dialogueFunction: PropTypes.func
 };
 
-export default ModalInfo;
+export default withStyles(styles)(ModalInfo);
