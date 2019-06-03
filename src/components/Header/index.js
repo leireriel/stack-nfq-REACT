6bc2f2903ec0f44479313659a13e8c8e792ff833
AiaRup <aiale88@gmail.com>
2019-06-03 12:26:54 +0200

    add link to header in stack list and question page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
-import './styles.scss';
 import React from 'react';
+import './styles.scss';
 import logoStackList from '../../images/logo-stackList.svg';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="container__header">
-        <img className="header__stackList" src={logoStackList} alt="Logotipo de nfq stack" />
+        <Link to="home">
+          <img className="header__stackList" src={logoStackList} alt="Logotipo de nfq stack" />
+        </Link>
       </div>
     );
   }
