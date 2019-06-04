e74d34afcd16d95c209e149c491ae77624f3210b
AiaRup <aiale88@gmail.com>
2019-06-04 21:41:35 +0200

    change url of api from localhost to the true url

--- src/services/fetchQuestion.js
+++ src/services/fetchQuestion.js
@@ -1,5 +1,4 @@
-const ENDPOINT = ' http://localhost:3001/questions';
-// const ENDPOINT = 'https://adalab.nfqsolutions.es/api/questions';
+const ENDPOINT = 'https://adalab.nfqsolutions.es/api/questions';
 
 const fetchQuestion = () => fetch(ENDPOINT).then(res => res.json());
 
